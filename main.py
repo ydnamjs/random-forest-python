6d5724194fca45bd3fba0669d7e9c0460f5475da
ydnamjs <ydnamjs@udel.edu>
2024-04-24 13:53:05 -0400

    Added data

---
+++ main.py
@@ -0,0 +1,19 @@
+features = [
+    "User Age", 
+    "User Description Length", 
+    "User Follower Count", 
+    "User Friends Count",
+    "User Favorites",
+    "User Lists",
+    "User Statuses Count",
+    "Hashtag Count",
+    "Mention Count",
+    "URL Count",
+    "Text Length",
+    "Text Digits",
+    "Retweeted Favorites Count",
+    "Is Truncated",
+    "User Is Default",
+    "Number Us"
+]
+

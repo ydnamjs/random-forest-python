5dfeec94ce80999615eb952aa6f808d600bb5f7d
michael <ydna.mjs@gmail.com>
2024-04-25 01:43:15 -0400

    Updated docstring for calcConditionalEntropy

--- decisionTree.py
+++ decisionTree.py
@@ -2,26 +2,22 @@ import math
 from dataclasses import dataclass
 from typing import Dict
 
-def calcConditionalEntropy(featureValuesData: list[Dict[int, int]])->float:
+def calcConditionalEntropy(valueLabelDicts: list[Dict[int, int]])->float:
     """
-        @param featureValuesData: Dict of feature value to Dict of label to frequency
-            For example we might have a feature: favorite animal where 0 is a dog, 1 is a cat, and 2 is a fish
-            And say we are classifying if they are a serial killer or not 0 is no 1 is yes
-            And say there are 6 dog lovers that are not serial killers, 3 dog lovers that are serial killers,
-            4 cat lovers that are not serial killers, 7 cat lovers that are serial killers,
-            5 fish lovers that are not serial killers, and 9 fish lovers that are serial killers
-            our featureValueData would then be {0: {0: 6, 1: 3;}; 1: {0: 4, 1: 7}, 2: {0: 5, 1: 9}}
+        @param valueLabelDicts: list of each feature's value classification amounts
+        ex: if our feature could be the value 0 or 1 and our label could be 3 or 4 and our data looked like
+        0 3; 0 3; 0 4; 1 4; 1 4; 1 3 our valueLabelDicts would be [{3: 2, 4: 1}, {3: 1, 4: 2}] because for value 0, 3 appears twice and 4 appears once
 
         @returns the entropy of splitting on this feature
     """
 
     totalInstances = 0
-    for value in featureValuesData:
+    for value in valueLabelDicts:
         for classAmt in value.values():
             totalInstances += classAmt
 
     entropy = 0
-    for value in featureValuesData:
+    for value in valueLabelDicts:
 
         valueFrequency = 0
         for numClassification in value.values():
